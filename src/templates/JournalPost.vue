a1c96595d0e0687f9ca87d6e0868395dc459b842
Prajjwal Uprety <prajjwal.uprety.3@gmail.com>
2020-09-16 19:52:25 +0545

    Update JournalPost.vue

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -29,7 +29,7 @@ query JournalPost ($path: String!) {
   post: journalPost (path: $path) {
     title
     author
-    date (format: "DD MMMM YYYY")
+    date (format: "D. MMMM YYYY")
     content
   }
 }
